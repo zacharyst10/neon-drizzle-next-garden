c762521982fe9c37399ed28a79e5ecff22d33e37
Zach Stout <stout.zachary@gmail.com>
2024-07-05 12:36:23 -0600

    added in navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { getData } from "@/actions/todo-actions";
 import Todos from "@/components/todos";
-import Image from "next/image";
 
 export default async function Home() {
   const data = await getData();
