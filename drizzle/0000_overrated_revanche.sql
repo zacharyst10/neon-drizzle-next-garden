78d3fb4834fa2bab3149bd1344330bea81907c85
Zach Stout <stout.zachary@gmail.com>
2024-07-07 20:12:05 -0600

    this better work

--- drizzle/0000_overrated_revanche.sql
+++ drizzle/0000_overrated_revanche.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
-	"clerk_id" text NOT NULL,
-	"first_name" text NOT NULL,
-	"last_name" text NOT NULL,
+	"clerkId" text NOT NULL,
+	"firstName" text NOT NULL,
+	"lastName" text NOT NULL,
 	"photo" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
