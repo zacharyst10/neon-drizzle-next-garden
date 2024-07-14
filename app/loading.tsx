e4afcbc1d8c4d6f210572e67c96aaddc7de69254
Zach Stout <stout.zachary@gmail.com>
2024-07-13 22:39:52 -0600

    added in the loader baby

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
+import Loader from '@/components/loader'
+
 export default function Loading() {
-  return <div>Loading...</div>
+  return (
+    <div>
+      <Loader size={100} />
+    </div>
+  )
 }
