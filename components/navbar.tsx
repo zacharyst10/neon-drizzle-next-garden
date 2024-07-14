e53eb55aa5e2625960a730016a08d1d93f341ad1
Zach Stout <stout.zachary@gmail.com>
2024-07-13 22:31:33 -0600

    adding in the cute icons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Image from 'next/image'
+import Link from 'next/link'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
@@ -12,6 +14,8 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger
 } from '@/components/ui/navigation-menu'
+
+import orange from '@/public/icons/orange.png'
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -60,19 +64,19 @@ export function Navbar() {
             <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
               <li className='row-span-3'>
                 <NavigationMenuLink asChild>
-                  <a
+                  <Link
                     className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
                     href='/'
                   >
                     <div className='mb-2 mt-4 text-lg font-medium'>
-                      shadcn/ui
+                      <Image src={orange} alt='logo' width={50} height={50} />
                     </div>
                     <p className='text-sm leading-tight text-muted-foreground'>
                       Beautifully designed components that you can copy and
                       paste into your apps. Accessible. Customizable. Open
                       Source.
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href='/encyclopedia' title='Encyclopedia'>
