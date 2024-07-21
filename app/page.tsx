674db815bb08d337924524025f965fcf255e18f4
Zach Stout <stout.zachary@gmail.com>
2024-07-21 12:00:00 -0600

    added in copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getUser } from '@/actions/user-actions'
 import { currentUser } from '@clerk/nextjs/server'
+import Link from 'next/link'
 
 import Todos from '@/components/todos'
 
@@ -11,6 +12,10 @@ export default async function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Garden Site</h1>
+      <div>
+        Not sure what to grow yet? Check out the{' '}
+        <Link href='/encyclopedia'>Encyclopedia</Link>
+      </div>
       <Todos todos={fetchedData[0].todos} user={fetchedData[0]} />
     </main>
   )
